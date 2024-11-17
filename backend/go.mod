de245070cdabf69959af2887b65855c29e80f253
Wade Cappa <wade.cappa@msn.com>
2024-11-17 10:04:31 -0500

    wip, rabbit might be better

--- backend/go.mod
+++ backend/go.mod
@@ -13,6 +13,11 @@ require (
 	github.com/bytedance/sonic/loader v0.2.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/confluentinc/confluent-kafka-go/v2 v2.6.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.7.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.6 // indirect
 	github.com/gin-contrib/cors v1.7.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -20,9 +25,19 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.1 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
@@ -31,7 +46,9 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.11.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -4,18 +4,20 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"os"
 	"sort"
 	"time"
 
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/lib/pq"
 	_ "github.com/lib/pq"
-	amqp "github.com/rabbitmq/amqp091-go"
 	xss "github.com/sahilchopra/gin-gonic-xss-middleware"
 )
+
+const kafkaTopic string = "message-events"
 
 type messagePostRequest struct {
 	Content string `form:"content" json:"content" xml:"content" binding:"required"`
@@ -28,28 +30,11 @@ type messageDeleteRequest struct {
 type Message struct {
 	Content    string
 	TimePosted time.Time
-	PostId     int64
+	PostId     uint64
 }
 
-type Event struct {
-	Name    string
-	Payload []byte
-}
-
-func runWithDb(consumer func(*sql.DB)) {
-	host := "db"
-	port := 5432
-	user := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PASSWORD")
-	dbname := os.Getenv("POSTGRES_DB")
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
-	consumer(db)
+func getRandomInt() uint64 {
+	return rand.Uint64()
 }
 
 func asMessages(messages *sql.Rows) []Message {
@@ -57,7 +42,7 @@ func asMessages(messages *sql.Rows) []Message {
 	for messages.Next() {
 		var content string
 		var timePosted int64
-		var postId int64
+		var postId uint64
 		if err := messages.Scan(&content, &timePosted, &postId); err == nil {
 			res = append(res, Message{
 				Content:    content,
@@ -74,100 +59,33 @@ func asMessages(messages *sql.Rows) []Message {
 	return res
 }
 
-func getMessages(postgres *sql.DB) ([]Message, error) {
-	res, err := postgres.Query("select content, time_posted, post_id from user_post")
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	defer res.Close()
-
-	return asMessages(res), nil
-}
-
-func deletePost(postgres *sql.DB, deleteRequest messageDeleteRequest) error {
-	_, err := postgres.Query("delete from user_post where user_post.post_id = any($1)", pq.Array(deleteRequest.PostIds))
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	return nil
-}
-
-func writePost(postgres *sql.DB, messagePost messagePostRequest, timePosted time.Time) ([]Message, error) {
-	post_id_conn, sequence_err := postgres.Query("select nextval('post_id_sequence')")
-	if sequence_err != nil {
-		fmt.Println(sequence_err)
-		return nil, sequence_err
-	}
-	var post_id int64
-	for post_id_conn.Next() {
-		if err := post_id_conn.Scan(&post_id); err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	fmt.Printf("got new id from database: %d\n", post_id)
-
-	_, err := postgres.Query("insert into user_post (time_posted, content, post_id) values ($1, $2, $3)", timePosted.UnixMilli(), messagePost.Content, post_id)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	return []Message{{
-		Content:    messagePost.Content,
-		TimePosted: timePosted,
-		PostId:     post_id,
-	}}, nil
-}
-
-func consumeRabbitEvents(rabbit *amqp.Connection, consumer func(amqp.Delivery) bool) {
-	readChannel, err := rabbit.Channel()
+func consumeEvents(consumer func([]byte) bool) {
+	groupId := string(getRandomInt())
+	kafkaConsumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "kafka",
+		"acks":              "all",
+		"auto.offset.reset": "earliest",
+		"group.id":          groupId,
+	})
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	defer readChannel.Close()
+	defer kafkaConsumer.Close()
 
-	queue, err := readChannel.QueueDeclare(
-		"",
-		false,
-		true,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer readChannel.QueueDelete(queue.Name, false, false, false)
-
-	bindError := readChannel.QueueBind(queue.Name, "", "events", false, nil)
-	if bindError != nil {
-		fmt.Println(bindError.Error())
-		return
-	}
-
-	msgs, err := readChannel.Consume(
-		queue.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
+	err = kafkaConsumer.SubscribeTopics([]string{kafkaTopic}, nil)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
 	for {
-		event := <-msgs
-		done := consumer(event)
+		msg, err := kafkaConsumer.ReadMessage(-1)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		done := consumer(msg.Value)
 		if done {
 			return
 		}
@@ -179,6 +97,16 @@ func main() {
 	var xssMdlwr xss.XssMw
 	r.Use(xssMdlwr.RemoveXss())
 	r.SetTrustedProxies(nil)
+
+	kafkaProducer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "kafka",
+		"acks":              "all",
+	})
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+	defer kafkaProducer.Close()
 
 	mode := os.Getenv("MODE")
 	config := cors.DefaultConfig()
@@ -197,39 +125,6 @@ func main() {
 	}
 	r.Use(cors.New(config))
 
-	rabbit, err := amqp.Dial("amqp://guest:guest@rabbitmq/")
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer rabbit.Close()
-
-	writeChannel, err := rabbit.Channel()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer writeChannel.Close()
-
-	exchangeError := writeChannel.ExchangeDeclare(
-		"events",
-		"fanout",
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if exchangeError != nil {
-		fmt.Println(exchangeError.Error())
-		return
-	}
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
 	r.DELETE("/", func(c *gin.Context) {
 		fmt.Println("received delete request")
 		var deleteRequest messageDeleteRequest
@@ -242,13 +137,6 @@ func main() {
 			fmt.Printf("deleting message %d\n", postId)
 		}
 
-		runWithDb(func(postgres *sql.DB) {
-			if err := deletePost(postgres, deleteRequest); err != nil {
-				c.JSON(http.StatusBadRequest, "Failed to delete post")
-				return
-			}
-		})
-
 		func() {
 			for _, p := range deleteRequest.PostIds {
 				payload := gin.H{"Payload": p, "Name": "deleteMessage"}
@@ -257,19 +145,11 @@ func main() {
 					fmt.Println(err.Error())
 					return
 				}
-				err = writeChannel.Publish(
-					"events",
-					"",
-					false,
-					false,
-					amqp.Publishing{
-						ContentType: "text/plain",
-						Body:        data,
-					})
-				if err != nil {
-					fmt.Println(err.Error())
-					return
-				}
+				topic := kafkaTopic
+				kafkaProducer.Produce(&kafka.Message{
+					TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+					Value:          []byte(data),
+				}, nil)
 			}
 		}()
 
@@ -287,50 +167,33 @@ func main() {
 		timePosted := time.Now()
 		fmt.Printf("received write request for message of '%s'\n", messagePost.Content)
 
-		runWithDb(func(postgres *sql.DB) {
-			messages, err := writePost(postgres, messagePost, timePosted)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, "Failed to write post")
-				return
-			}
-			func() {
-				for _, m := range messages {
-					payload := gin.H{"Payload": m, "Name": "newMessage"}
-					data, err := json.Marshal(payload)
-					if err != nil {
-						fmt.Println(err.Error())
-						return
-					}
-					err = writeChannel.Publish(
-						"events",
-						"",
-						false,
-						false,
-						amqp.Publishing{
-							ContentType: "text/plain",
-							Body:        data,
-						})
-					if err != nil {
-						fmt.Println(err.Error())
-						return
-					}
-				}
-			}()
-		})
+		message := Message{
+			Content:    messagePost.Content,
+			TimePosted: timePosted,
+			PostId:     getRandomInt(),
+		}
 
-		c.Status(http.StatusOK)
+		payload := gin.H{"Payload": message, "Name": "newMessage"}
+		data, err := json.Marshal(payload)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		topic := kafkaTopic
+		resultChannel := make(chan kafka.Event)
+		kafkaProducer.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic},
+			Value:          []byte(data),
+		}, resultChannel)
+
+		kafkaProducer.Flush(999999999)
+		res := <-resultChannel
+		c.JSON(http.StatusOK, res)
 	})
 
 	r.GET("/", func(c *gin.Context) {
 		fmt.Println("received get request")
-		runWithDb(func(postgres *sql.DB) {
-			res, err := getMessages(postgres)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, "Failed to get posts")
-				return
-			}
-			c.JSON(http.StatusOK, res)
-		})
+		c.Status(http.StatusOK)
 	})
 
 	r.GET("/watch", func(c *gin.Context) {
@@ -339,7 +202,7 @@ func main() {
 		c.Writer.Flush()
 		fmt.Println("connected with client")
 
-		consumeRabbitEvents(rabbit, func(newEvent amqp.Delivery) bool {
+		consumeEvents(func(newEvent []byte) bool {
 			select {
 			case <-c.Request.Context().Done():
 				// exit when done
@@ -347,7 +210,7 @@ func main() {
 			default:
 				// no-op, keep going
 			}
-			c.Writer.Write([]byte(fmt.Sprintf("data: %s\n", newEvent.Body)))
+			c.Writer.Write([]byte(fmt.Sprintf("data: %s\n", newEvent)))
 			c.Writer.Write([]byte("\n"))
 			c.Writer.Flush()
 
